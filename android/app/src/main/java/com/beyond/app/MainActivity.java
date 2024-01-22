6169269264c3074a6f05712e666cfea29ca0d068
machineke <markarapsoi72@gmail.com>
2024-01-22 03:05:57 +0300

    job search app

--- android/app/src/main/java/com/beyond/app/MainActivity.java
+++ android/app/src/main/java/com/beyond/app/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.app;
+package com.beyond.app;
 
 import com.getcapacitor.BridgeActivity;
 
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,12 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'com.beyond.app',
-  appName: 'jobsearchapp',
-  webDir: 'build',
+  appId: "com.beyond.app",
+  appName: "jobSearchApp",
+  webDir: "build",
   server: {
-    androidScheme: 'https'
-  }
+    androidScheme: "https",
+  },
 };
 
 export default config;
