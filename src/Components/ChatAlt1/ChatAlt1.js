c79f22326f0ea91ac0d72b8e16eb61d7b25a7067
machineke <markarapsoi72@gmail.com>
2024-02-11 14:00:45 +0300

    hosting some servers

--- src/Components/ChatAlt1/ChatAlt1.js
+++ src/Components/ChatAlt1/ChatAlt1.js
@@ -1,17 +1,14 @@
-import { useState, useContext } from "react";
+import { useState } from "react";
 
 import "./ChatAlt1.css"
 import AuthPage from "./AuthPage";
 import ChatsPage from "./ChatsPage";
-import { AuthContext } from "../Posting/Client/Context/auth";
-import PostLogin from '../Posting/Client/PostLogin'
-function ChatAlt1() {
-const user = useContext(AuthContext)
 
-  // const [user, setUser] = useState(undefined);
+function ChatAlt1() {
+  const [user, setUser] = useState(undefined);
 
   if (!user) {
-    return <PostLogin/>;
+    return <AuthPage onAuth={(user) => setUser(user)} />;
   } else {
     return <ChatsPage user={user} />;
   }
