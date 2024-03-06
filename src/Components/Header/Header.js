571e857059a9f3aeb030e5677974fb8b8cb175d2
machineke <markarapsoi72@gmail.com>
2024-03-06 22:03:45 +0300

    fixed header issue

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,23 +3,33 @@ import { AuthContext } from "../Posting/Client/Context/auth";
 
 const Header = () => {
   const { user, logout } = useContext(AuthContext);
-  const topBar = user ? (
+  // const topBar = user ? (
+  //   <div className="navBar">
+  //     <div className="logoDiv">
+  //       <img src="https://res.cloudinary.com/dmpposta9/image/upload/v1709168114/careerQuest/img/azfdzi2gflcyu8ijquy8.png" alt="logo" className="appLogo "/>
+  //       <p className="headerInNav headerName">CareerQuest</p>
+  //       {/* <p>{user.username}</p> */}
+  //     </div>
+  //   </div>
+  // ) : (
+  //   <div className="navBar">
+  //     <div className="logoDiv">
+  //       <img src="https://res.cloudinary.com/dmpposta9/image/upload/v1709168114/careerQuest/img/azfdzi2gflcyu8ijquy8.png" alt="logo" className="appLogo "/>
+  //       <p className="logo">CareerQuest</p>
+  //     </div>
+  //   </div>
+  // );
+  // return topBar;
+  return (
+    <>
     <div className="navBar">
-      <div className="logoDiv">
+       <div className="logoDiv">
         <img src="https://res.cloudinary.com/dmpposta9/image/upload/v1709168114/careerQuest/img/azfdzi2gflcyu8ijquy8.png" alt="logo" className="appLogo "/>
-        <p className="headerInNav headerName">CareerQuest</p>
-        {/* <p>{user.username}</p> */}
-      </div>
+       <p className="headerInNav headerName">CareerQuest</p>
+       </div>
     </div>
-  ) : (
-    <div className="navBar">
-      <div className="logoDiv">
-        <img src="https://res.cloudinary.com/dmpposta9/image/upload/v1709168114/careerQuest/img/azfdzi2gflcyu8ijquy8.png" alt="logo" className="appLogo "/>
-        <p className="logo">CareerQuest</p>
-      </div>
-    </div>
-  );
-  return topBar;
+    </>
+  )
 };
 
 export default Header;
